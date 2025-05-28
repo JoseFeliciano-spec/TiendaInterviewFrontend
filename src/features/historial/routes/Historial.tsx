9e89b22d6a6838263cd1522cd930bcbdff5d4594
Jose Feliciano <janayasimanca@gmail.com>
2025-05-28 03:16:07 -0500

    fix: Free dependency

--- src/features/historial/routes/Historial.tsx
+++ src/features/historial/routes/Historial.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
